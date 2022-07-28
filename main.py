586c60e0a9b0f555404111f109863e073f4df0c7
GoodVessel92551 <100193053+GoodVessel92551@users.noreply.github.com>
2022-07-28 18:50:14 +0000

    (BETA) Write Page added

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ from replit import web, db
 app = Flask(__name__)
 users = web.UserStore()
 
+email = ["GoodVessel92551","test","test","bob","test2","test2","jeff","test3","test3"]
+name = "jeff"
 @app.route('/')
 def index():
+    newmail = []
+    for i in range(len(email)):
+        if email[i] == name and i%3 == 0:
+            newmail.append(email[i])
+            newmail.append(email[i+1])
+            newmail.append(email[i+2])
+            print(newmail)
+            i =+ 2
     return render_template("index.html")
 
 @app.route('/home')
 @web.authenticated
 def home():
     return render_template("home.html")
+
+@app.route('/write')
+@web.authenticated
+def write():
+    return render_template("send.html")
 
 web.run(app, port=8080, debug=True)
