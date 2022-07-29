ed5be038ecfeb8c0ecc6b847c58032f2b8d08fa2
GoodVessel92551 <100193053+GoodVessel92551@users.noreply.github.com>
2022-07-29 12:23:46 +0000

    Sending(BETA)

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
-from flask import Flask, render_template
+from better_profanity import profanity
+from flask import Flask, render_template, redirect, request, current_app
 from replit import web, db
 
 app = Flask(__name__)
 users = web.UserStore()
-
-email = ["GoodVessel92551","test","test","bob","test2","test2","jeff","test3","test3"]
-name = "jeff"
+names = ["GoodVessel92551","bob","jeff"]
+name = "GoodVessel92551"
 @app.route('/')
 def index():
-    newmail = []
-    for i in range(len(email)):
-        if email[i] == name and i%3 == 0:
-            newmail.append(email[i])
-            newmail.append(email[i+1])
-            newmail.append(email[i+2])
-            print(newmail)
-            i =+ 2
     return render_template("index.html")
 
 @app.route('/home')
 @web.authenticated
 def home():
-    return render_template("home.html")
+    email = db["mail"]
+    newmail = users.current["mails"]
+    for i in range(len(email)):
+        if i < len(email):
+            if email[i] == web.auth.name.lower() and i%4 == 0:
+                print(email[i])
+                users.current["mails"].append(email[i])
+                users.current["mails"].append(email[i+1])
+                users.current["mails"].append(email[i+2])
+                users.current["mails"].append(email[i+3])
+                email.pop(i)
+                email.pop(i)
+                email.pop(i)
+                email.pop(i)
+                print(email)
+                db["mail"] = email
+                print(db["mail"])
+            else:
+                break
+    return render_template("home.html",newmail=users.current["mails"])
 
-@app.route('/write')
+@app.route('/write', methods=["POST", "GET"])
 @web.authenticated
 def write():
-    return render_template("send.html")
+    mail = db["mail"]
+    if request.method == "POST":
+        if request.form["to"] not in names:
+            return "User dose not exsite on Booogle Mail"
+        elif len(request.form["about"]) > 140 or len(request.form["about"]) < 1:
+            return "Mail was to long or to short"
+        if len(request.form["desc"]) > 1500 or len(request.form["desc"]) < 10:
+            return "Mail was to long or to short"
+        else:
+            mail.append(request.form["to"].lower())
+            mail.append(web.auth.name)
+            mail.append(request.form["about"])
+            mail.append(request.form["desc"])
+        print(mail)
+        return redirect("/home")
+    else:
+        return render_template("send.html")
 
 web.run(app, port=8080, debug=True)
